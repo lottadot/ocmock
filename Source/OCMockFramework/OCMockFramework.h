9bdacbb05359507df2a8fe8e7e190d7d64bceea5
Shane Zatezalo <shane@lottadot.com>
2015-05-03 23:03:26 -0400

    Add initial Carthage support

---
+++ Source/OCMockFramework/OCMockFramework.h
@@ -0,0 +1,29 @@
+//
+//  OCMockFramework.h
+//  OCMockFramework
+//
+//  Created by Shane Zatezalo on 5/3/15.
+//  Copyright (c) 2015 Mulle Kybernetik. All rights reserved.
+//
+
+@import Foundation;
+@import UIKit;
+
+//! Project version number for OCMockFramework.
+FOUNDATION_EXPORT double OCMockFrameworkVersionNumber;
+
+//! Project version string for OCMockFramework.
+FOUNDATION_EXPORT const unsigned char OCMockFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OCMockFramework/PublicHeader.h>
+
+#import <OCMockFramework/OCMock.h>
+#import <OCMockFramework/OCMockObject.h>
+#import <OCMockFramework/OCMRecorder.h>
+#import <OCMockFramework/OCMStubRecorder.h>
+#import <OCMockFramework/OCMConstraint.h>
+#import <OCMockFramework/OCMArg.h>
+#import <OCMockFramework/OCMLocation.h>
+#import <OCMockFramework/OCMMacroState.h>
+#import <OCMockFramework/NSNotificationCenter+OCMAdditions.h>
+
